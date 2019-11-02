ca07587f58b4337a83774fcb98cb716571f329c6
hraya <hraya@lssu.edu>
2019-11-02 13:49:38 -0500

    Initial commit
    Facebook login button functional

---
+++ frontend/first-contact/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import LoginButton from './components/LoginButton';
+
+
+function App() {
+  return (
+    <div className="App">
+      <LoginButton />
+    </div>
+  );
+}
+
+export default App;
